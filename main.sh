8fb69453cd7196e53221d9c2f533c49066f9a38d
Max <massariolmc@gmail.com>
2018-11-29 18:33:57 -0300

    terceira vez

--- main.sh
+++ main.sh
@@ -15,6 +15,18 @@ bkp1='/home/massariol/Documentos/BACKUPS/TESTE1/'
 bkp2='/home/massariol/Documentos/BACKUPS/TESTE2/'
 bkp3='/home/massariol/Documentos/BACKUPS/TESTE3/'
 
+####
+bkp1=(quarta teste1.txt '/home/massariol/Documentos/BACKUPS/TESTE1')
+bkp2=(quarta teste2.txt '/home/massariol/Documentos/BACKUPS/TESTE2')
+bk3=(quarta teste3.txt '/home/massariol/Documentos/BACKUPS/TESTE3')
+bk4=(quarta teste4.txt '/home/massariol/Documentos/BACKUPS/TESTE1')
+bk5=(quarta teste5.txt '/home/massariol/Documentos/BACKUPS/TESTE2')
+#bk6=(quarta teste1.txt '/home/massariol/Documentos/BACKUPS/TESTE1')
+#bk7=(quarta teste1.txt '/home/massariol/Documentos/BACKUPS/TESTE1')
+#bkp8=(quarta teste1.txt '/home/massariol/Documentos/BACKUPS/TESTE1')
+#bkp9=(quarta teste1.txt '/home/massariol/Documentos/BACKUPS/TESTE1')
+#bkp10=(quarta teste1.txt '/home/massariol/Documentos/BACKUPS/TESTE1')
+
 #IP do servidor de backup
 ip="192.168.0.100"
 user="massariol"
@@ -31,7 +43,7 @@ seleciona_arq(){
 	if [ -e $1 ];then
 		echo "Arquivo Existe"
 	#	scp $1 massariol@192.168.0.102:$bkp1 | echo -e "12345678\n"
-		rsync avzhEP $1 massariol@192.168.0.102:$bkp3/
+		rsync -uavzhEP $1 massariol@192.168.0.103:$bkp3/
 
 		if [ $? -eq 0 ];then
 			echo "Deu certo"
@@ -42,6 +54,27 @@ seleciona_arq(){
 		echo "Arquivo não existe"
 	fi	
 }
+
+seleciona_bkp(){
+	data=verifica_data
+	echo "caminho $2"
+
+	cd $c1
+	if [ -e $2 ];then
+		echo "Arquivo Existe"
+	#	scp $1 massariol@192.168.0.102:$bkp1 | echo -e "12345678\n"
+		rsync -uavzhEP $2 massariol@192.168.0.103:$3/
+
+		if [ $? -eq 0 ];then
+			echo "Deu certo"
+		else
+			echo "Erro na cópia"
+		fi	
+	else
+		echo "Arquivo não existe"
+	fi	
+}
+
 
 copia_arq(){
 	arq=$@
@@ -68,9 +101,10 @@ verifica_dia(){
 			copia_arq ${arq2[@]}
 		elif [ $dia = "terça" ]; then
 			echo 3
-			copia_arq ${arq3[@]} $bkp3
+			copia_arq ${arq3[@]}
 		elif [ $dia = "quarta" ]; then
 			echo 4
+			seleciona_bkp ${bkp[@]}
 		elif [ $dia = "quinta" ]; then
 			echo 5
 		elif [ $dia = "sexta" ]; then
